ee5bf4f5db8f194683b81567bddc55da2ec4858c
Gerald Isabelle <gerald.isabelle@cohaesus.co.uk>
2016-04-07 14:46:52 +0100

    update gulp and package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,45 +1,19 @@
 import gulp from 'gulp';
-import uglify from 'gulp-uglify';
-import concat from 'gulp-concat';
 import babel from 'gulp-babel';
 import watch from 'gulp-watch';
 
-
-/* Environment Path */
 const path = {
     dist: "dist/",
-    src: "src/",
-    js: function() { return this.src + "js/" },
-    BuildJs: function() { return this.dist + "js/" },
+    src: "src/"
 };
 
-// concat js files
-gulp.task('concat', () => {
-    return gulp.src(path.js() + '**/*.js')
-        .pipe(concat({ path: 'new.js', stat: { mode: 666 }}))
-        .pipe(gulp.dest(path.BuildJs()));
-});
-
-gulp.task('compress', () => {
-    return gulp.src(path.js() + '**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest(path.BuildJs()));
-});
-
-
-// watch changes
-gulp.task('watch', () => {
-    //gulp.watch( path.js() + '**/*.js', ['compress']);
-    gulp.watch( path.js() + '**/*.js', ['concat']);
-});
-
 gulp.task('stream', () => {
-    gulp.src('src/**/*.js')
-        .pipe(watch('src/**/*.js'))
+    gulp.src(path.src+'**/*.js')
+        .pipe(watch(path.src+'**/*.js'))
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest(path.dist))
 });
 
 
